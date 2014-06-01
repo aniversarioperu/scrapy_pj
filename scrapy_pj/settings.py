efcf2c97dbac60afe72b478685c6f112d6b60285
aniversarioperu <aniversarioperu1@gmail.com>
2014-06-01 10:00:03 +0300

    start scrapy

---
+++ scrapy_pj/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for scrapy_pj project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scrapy_pj'
+
+SPIDER_MODULES = ['scrapy_pj.spiders']
+NEWSPIDER_MODULE = 'scrapy_pj.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapy_pj (+http://www.yourdomain.com)'
