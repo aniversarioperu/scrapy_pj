0a743fd5d46f2a4dfcce7d37a9e5009a465d8006
aniversarioperu <aniversarioperu1@gmail.com>
2014-06-02 10:21:32 +0300

    use cookiejar

--- scrapy_pj/spiders/spider_pj.py
+++ scrapy_pj/spiders/spider_pj.py
@@ -9,20 +9,23 @@ class Search_PJ(Spider):
     name = "search_pj"
     allowed_domains = ["jurisprudencia.pj.gob.pe"]
     start_urls = [
-        "http://jurisprudencia.pj.gob.pe/jurisprudenciaweb/faces/page/resolucion-busqueda-resultado.xhtml",
+        "http://jurisprudencia.pj.gob.pe/jurisprudenciaweb/faces/page/resolucion-busqueda-general.xhtml",
     ]
 
     def parse(self, response):
         cookieJar = response.meta.setdefault('cookie_jar', CookieJar())
         cookieJar.extract_cookies(response, response.request)
         mycookie = cookieJar._cookies['jurisprudencia.pj.gob.pe']['/jurisprudenciaweb']['JSESSIONID']
-        return [FormRequest(url=self.start_urls[0],
+        request = FormRequest(url=self.start_urls[0],
             formdata={
-                'formBusqueda:buEspecialidadInput': 'Civil',
-                'formBusqueda:buAnioInput':'2013',
+                'formBusqueda:buNoExpediente': '000001-2013',
                 },
+            #cookies={'JSESSIONID': str(mycookie.value)},
+            meta={'dont_merge_cookies': True, 'cookie_jar': cookieJar},
             callback=self.after_search)
-            ]
+        cookieJar.add_cookie_header(request)
+        yield request
+            
 
     def after_search(self, response):
         if 'authentication failed' in response.body:
