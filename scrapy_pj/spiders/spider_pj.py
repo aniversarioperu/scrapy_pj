a5552efc9032ccb18c39a1b1c17a91a493a5b5bc
aniversarioperu <aniversarioperu1@gmail.com>
2014-06-02 12:17:14 +0300

    works, sort of

--- scrapy_pj/spiders/spider_pj.py
+++ scrapy_pj/spiders/spider_pj.py
@@ -2,42 +2,56 @@ import json
 
 from scrapy.spider import Spider
 from scrapy.http import FormRequest
+from scrapy.http import Request
 from scrapy.http.cookies import CookieJar
 
 
 class Search_PJ(Spider):
     name = "search_pj"
     allowed_domains = ["jurisprudencia.pj.gob.pe"]
-    start_urls = [
-        "http://jurisprudencia.pj.gob.pe/jurisprudenciaweb/faces/page/resolucion-busqueda-general.xhtml",
-    ]
+    target_url = "http://jurisprudencia.pj.gob.pe/jurisprudenciaweb/faces/page/resolucion-busqueda-resultado.xhtml"
+
+    def start_requests(self):
+        #cookieJar = response.meta.setdefault('cookie_jar', CookieJar())
+        #cookieJar.extract_cookies(response, response.request)
+        #mycookie = cookieJar._cookies['jurisprudencia.pj.gob.pe']['/jurisprudenciaweb']['JSESSIONID']
+        #print "my cookie:\t", mycookie
+        yield Request(self.target_url, cookies={'JSESSIONID': 'S99PW04VLX~zc+5l2lit5jlYB1zBNQvvYYy.86439558-da66-3a1b-af8b-0355e5d4e948'},
+            callback=self.parse)
+
+        """
+        cookieJar.add_cookie_header(request)
+        print "<br><br>---<br>"
+        print "request.url,", request.url, "<br>"
+        print "request.callback,", request.callback, "<br>"
+        print "request.method,", request.method, "<br>"
+        print "request.meta,", request.meta, "<br>"
+        print "request.body,", request.body, "<br>"
+        print "request.headers,", request.headers, "<br>"
+        print "request.cookies,", request.cookies, "<br>"
+        print "request.encoding,", request.encoding, "<br>"
+        print "request.priority,", request.priority, "<br>"
+        print "request.dont_filter,", request.dont_filter, "<br>"
+        print "request.errback,", request.errback, "<br>"
+        """
 
     def parse(self, response):
-        print response.headers
-        cookieJar = response.meta.setdefault('cookie_jar', CookieJar())
-        cookieJar.extract_cookies(response, response.request)
-        request = FormRequest(url=self.start_urls[0],
+        if 'authentication failed' in response.body:
+            self.log("Search failed", level=log.ERROR)
+            print response
+            return
+
+        print response.headers, "<br><br>"
+        print response.meta, "<br><br>"
+        print response.body, "<br><br>"
+
+        return [FormRequest(url=self.start_urls[0],
             formdata={
-                'formBusqueda:buNoExpediente': '000001-2013',
+                 'formBusqueda:buEspecialidadInput': 'Civil',
+                 'formBusqueda:buAnioInput':'2013',
                 },
-            #cookies={'JSESSIONID': str(mycookie.value)},
-            meta={'dont_merge_cookies': True, 'cookie_jar': cookieJar},
-            callback=self.after_search)
-        print "<br><br>---<br>"
-        print request.url, "<br>"
-        print request.callback, "<br>"
-        print request.method, "<br>"
-        print request.meta, "<br>"
-        print request.body, "<br>"
-        print request.headers, "<br>"
-        print request.cookies, "<br>"
-        print request.encoding, "<br>"
-        print request.priority, "<br>"
-        print request.dont_filter, "<br>"
-        print request.errback, "<br>"
-        cookieJar.add_cookie_header(request)
-        yield request
-            
+            cookies=[{'name':'JSESSIONID', 'value': 'S99PW04VLX~zc+5l2lit5jlYB1zBNQvvYYy.86439558-da66-3a1b-af8b-0355e5d4e948'}],
+            callback=self.after_search)]
 
     def after_search(self, response):
         if 'authentication failed' in response.body:
@@ -45,5 +59,6 @@ class Search_PJ(Spider):
             print response
             return
 
-        print response.body
-
+        print response.headers, "<br><br>"
+        print response.meta, "<br><br>"
+        print response.body, "<br><br>"
