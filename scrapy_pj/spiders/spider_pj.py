613fb43f3744af06179aa1614ab785a0edd5a7c8
aniversarioperu <aniversarioperu1@gmail.com>
2014-06-04 19:35:36 +0300

    successfull post request

--- scrapy_pj/spiders/spider_pj.py
+++ scrapy_pj/spiders/spider_pj.py
@@ -10,20 +10,25 @@ from scrapy.http.cookies import CookieJar
 class Search_PJ(Spider):
     name = "search_pj"
     allowed_domains = ["jurisprudencia.pj.gob.pe"]
-    target_url = "http://jurisprudencia.pj.gob.pe/jurisprudenciaweb/faces/page/resolucion-busqueda-resultado.xhtml"
+    target_url = "http://jurisprudencia.pj.gob.pe/jurisprudenciaweb/faces/page/resolucion-busqueda-general.xhtml"
 
     def start_requests(self):
         yield Request(self.target_url,  callback=self.parse)
 
     def parse(self, response):
-        print "<br>response.headers", response.headers
         return [FormRequest.from_response(response,
                 formdata={
-                  'formBusqueda:buEspecialidadInput': 'Civil',
-                  'formBusqueda:buAnioInput':'2013',
+                    'formBusqueda':'formBusqueda',
+                    'formBusqueda:txtBusqueda':'Ingrese el texto a buscar',
+                    'formBusqueda:cmbCorte':'',
+                    'formBusqueda:buNroExpediente':'000001-2013',
+                    'javax.faces.source':'formBusqueda:j_idt30',
+                    'forward':'buscar',
+                    'formBusqueda:j_idt32':'21',
+                    'formBusqueda:j_idt33':'ASC',
+                    'org.richfaces.ajax.component':'formBusqueda:j_idt30',
                     },
-                dont_click=True,
-                #cookies=[self.make_cookie(response)],
+                clickdata={'id': 'formBusqueda:j_idt30',},
                 callback = self.after_search)]
 
     def after_search(self, response):
