83968bae76d9bddfe1ba5822181aefebf0917bca
aniversarioperu <aniversarioperu1@gmail.com>
2014-06-04 23:04:54 +0300

    selecting inputs

--- scrapy_pj/spiders/spider_pj.py
+++ scrapy_pj/spiders/spider_pj.py
@@ -1,7 +1,9 @@
 import json
+import re
 import sys
 
 from scrapy.spider import Spider
+from scrapy.selector import Selector
 from scrapy.http import FormRequest
 from scrapy.http import Request
 from scrapy.http.cookies import CookieJar
@@ -34,4 +36,20 @@ class Search_PJ(Spider):
         )]
 
     def after_search(self, response):
-        print "<br>response.body", response.body
+        sel = Selector(response)
+        ls = sel.xpath('//input[starts-with(@value, "Ver Resolu")]').extract()
+        patt = "(formBusqueda:repeat:[0-9]+:j_idt[0-9]+).+uuid\W+((\w+-)+\w+)"
+        for l in ls:
+            res = re.search(patt, l).groups()
+            if res:
+                idt = res[0]
+                uuid = res[1]
+                print idt, uuid, "<br>"
+        #print "<br>response.body", response.body
+
+
+
+# post request to download PDF file
+"""
+        formBusqueda=formBusqueda&formBusqueda%3AbuCorte=&formBusqueda%3Aj_idt25-value=j_idt26&formBusqueda%3AbuEspecialidad=0&formBusqueda%3AbuEspecialidadInput=--+Todos+--&formBusqueda%3AbuPretensionValue=&formBusqueda%3AbuPretensionInput=&formBusqueda%3AbuPalabraClaveValue=&formBusqueda%3AbuPalabraClaveInput=&formBusqueda%3AbuSala=0&formBusqueda%3AbuSalaInput=--+Todos+--&formBusqueda%3AbuTipoRecurso=0&formBusqueda%3AbuTipoRecursoInput=--+Todos+--&formBusqueda%3AbuTipoResolucion=0&formBusqueda%3AbuTipoResolucionInput=--+Todos+--&formBusqueda%3AbuAnio=&formBusqueda%3AbuAnioInput=--+Seleccione+--&formBusqueda%3AtxtBusqueda=&formBusqueda%3AbuOrden=21&formBusqueda%3AbuOrdenInput=Fecha+Resoluci%C3%B3n&formBusqueda%3AbuOrdenForma=ASC&formBusqueda%3AbuOrdenFormaInput=Ascendente&formBusqueda%3AbuPaginas=10&formBusqueda%3AbuPaginasInput=10+resultados&javax.faces.ViewState=-697870706530378361%3A6617078509790886010&formBusqueda%3Arepeat%3A0%3Aj_idt158=formBusqueda%3Arepeat%3A0%3Aj_idt158&uuid=47cd6b37-8c7b-4cd0-b46a-adb4755bb161
+"""
