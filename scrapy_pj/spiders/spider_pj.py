480f0a6cf20a2be91d45a7a8ace9c7c3b80b4b31
aniversarioperu <aniversarioperu1@gmail.com>
2014-06-04 19:38:59 +0300

    pep8

--- scrapy_pj/spiders/spider_pj.py
+++ scrapy_pj/spiders/spider_pj.py
@@ -16,55 +16,22 @@ class Search_PJ(Spider):
         yield Request(self.target_url,  callback=self.parse)
 
     def parse(self, response):
-        return [FormRequest.from_response(response,
-                formdata={
-                    'formBusqueda':'formBusqueda',
-                    'formBusqueda:txtBusqueda':'Ingrese el texto a buscar',
-                    'formBusqueda:cmbCorte':'',
-                    'formBusqueda:buNroExpediente':'000001-2013',
-                    'javax.faces.source':'formBusqueda:j_idt30',
-                    'forward':'buscar',
-                    'formBusqueda:j_idt32':'21',
-                    'formBusqueda:j_idt33':'ASC',
-                    'org.richfaces.ajax.component':'formBusqueda:j_idt30',
-                    },
-                clickdata={'id': 'formBusqueda:j_idt30',},
-                callback = self.after_search)]
+        return [FormRequest.from_response(
+            response,
+            formdata={
+                'formBusqueda': 'formBusqueda',
+                'formBusqueda:txtBusqueda': 'Ingrese el texto a buscar',
+                'formBusqueda:cmbCorte': '',
+                'formBusqueda:buNroExpediente': '000001-2013',
+                'javax.faces.source': 'formBusqueda:j_idt30',
+                'forward': 'buscar',
+                'formBusqueda:j_idt32': '21',
+                'formBusqueda:j_idt33': 'ASC',
+                'org.richfaces.ajax.component': 'formBusqueda:j_idt30',
+            },
+            clickdata={'id': 'formBusqueda:j_idt30', },
+            callback=self.after_search
+        )]
 
     def after_search(self, response):
-        print "<br>response.headers", response.headers
         print "<br>response.body", response.body
-
-    def make_cookie(self, response):
-        tmp = response.headers['Set-Cookie']
-        tmp = tmp.split(";")[0]
-        tmp = tmp.split("=")[1]
-        mycookies = []
-        cookies = {}
-        cookies['name'] = 'JSESSIONID'
-        cookies['value'] = tmp
-        cookies['domain'] = "jurisprudencia.pj.gob.pe"
-        cookies['path'] = "/jurisprudenciaweb"
-        """
-        mycookies.append(cookies)
-        tmp = {'name': '__utma',
-                'value':'224540507.1467503965.1401198857.1401198857.1401222458.2',
-                'domain': '.pj.gob.pe',
-                'path': '/'}
-        mycookies.append(tmp)
-        tmp = {'name': '__utmz',
-                'value':'224540507.1401222458.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=http://www.pj.gob.pe/',
-                'domain': '.pj.gob.pe',
-                'path': '/'}
-        mycookies.append(tmp)
-
-        cookies = {}
-        cookies['name'] = 'JSESSIONID'
-        tmp = tmp.split(";")
-        cookies['value'] = tmp[0].split("=")[1]
-        cookies['domain'] = "jurisprudencia.pj.gob.pe"
-        cookies['path'] = "/jurisprudenciaweb"
-        """
-        print "cookies:->", cookies
-        return cookies
-        #return mycookies
